d6ccd15357996dfb82a86eb2b0516424c4bd0a08
Emma Rand <emma.rand@york.ac.uk>
2022-02-02 11:58:05 +0000

    add a couple of comments

--- R/analysis.R
+++ R/analysis.R
@@ -28,6 +28,8 @@ patients %>% ggplot(aes(x = age, y = n_wbv, color = group)) +
 # demented have lower nbv
 
 
+
+
 patients %>% ggplot(aes(x = e_tiv, y = n_wbv, color = group)) +
   geom_point()
 
@@ -36,3 +38,29 @@ table(patients$cdr)
 
 patients %>% ggplot(aes(x = cdr, y = age)) +
   geom_boxplot()
+
+patients %>% filter(is.na(mmse))
+patients %>% filter(subject_id == "OAS2_0181")
+
+
+patients %>% filter(visit == 1) %>%
+  ggplot(aes(x = ses, y = educ)) +
+  geom_point(alpha = 0.1)
+
+
+# remove
+#    ses because it has 19 missing values
+#    the two rows with missing mmse values (one patient)
+#    remove uninformative values
+patients_noses <- patients %>%
+  select(-ses, -hand, -asf) %>%
+  filter(!is.na(mmse))
+
+summary(patients_noses)
+
+library(GGally)
+patients_noses %>%
+  select(-subject_id, -mri_id) %>%
+  ggpairs()
+
+patients_noses %>% group_by(group, m_f) %>% count()
